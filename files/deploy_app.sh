b1af6a68ea6114da5aa54c868b4913c4ce3ca97f
서용준 <syj0422@gachon.ac.kr>
2024-05-24 14:06:03 +0900

    edit size

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="https://raw.githubusercontent.com/mango0422/hashicat-app/main/files/ProFit_member.jpg" max-height="1080" ></img></center>
+  <center><img src="https://raw.githubusercontent.com/mango0422/hashicat-app/main/files/ProFit_member.jpg" height="600" ></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to team ProFit! We're glad to see you.<br/>
   We'll be
